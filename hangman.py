398b5a58f66b33e836d6adceb50728bb2108839c
prernagulati0 <67996489+prernagulati0@users.noreply.github.com>
2020-07-14 01:33:06 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,9 +1,10 @@
+#main function
 def hangman():
     global picked,right,n,entryy,wrong,x,temp,chances
     guess = inp.get()
     entry.delete(0,END)
     
-    if(n>1):
+    if(n>1):      
         if guess in picked:
             for i in range(len(picked)):
                 while picked[i]==guess :
@@ -38,8 +39,6 @@ def hangman():
         label.place(x=590,y=150)
         label.update(photo3)
        
-       
-        
 def jj(event):
     hangman()  
 
@@ -50,12 +49,11 @@ def b():
     label.place(x=590,y=150)
     label.update(photo3)
     
-        
+#import modules       
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 import random
-
 
 root = Tk()
 root.geometry('1200x670+90+15')
@@ -66,6 +64,7 @@ words = ['titanic','cindrella','goodfellas','vertigo','alien','batman','joker','
          'bambi','shrek','inception','predator','matilda','memento','cocktail','parasite','fanaa','kahani','hungama',
         'zero','deadpool','dangal','mowgli','baaghi','war','barfi','pk','raees','fashion','sanju,super30']
 
+#labels
 f= Label(root,text='Guess the movie-',font=('oswal',17,'bold'),bg='#f0f0f0')
 f.place(x=120,y=200)
 intro = Label(root,text='Welcome to HAngman game',font=('oswal',20,'bold'),bg='#f0f0f0')
@@ -80,10 +79,12 @@ result = Label(root,text='You won!!',font=('oswal',15,'bold'),bg='#f0f0f0')
 result.place(x=345,y=520)
 z= Label(root,text='',font=('oswal',12,'bold'),bg='#f0f0f0')
 z.place(x=365,y=550)
+#entry box
 inp = StringVar()
 entry = Entry(root,font=('oswal',25,'bold'),relief=RIDGE,bd=3,textvariable=inp, width=3)
 entry.focus_set()
 entry.place(x=180,y=450)
+#buttons
 bt1 = Button(root,text='Enter',font=('oswal',15,'bold'),width=7,bd=4,bg='black',relief=RIDGE 
              ,fg='white',command=hangman)
 bt1.place(x=260,y=450)
@@ -94,7 +95,6 @@ bt2.place(x=460,y=595)
 bt3 = Button(root,text='restart',font=('oswal',15,'bold'),width=7,bd=5,bg='black',relief=RIDGE 
              ,fg='white',command=b)
 bt3.place(x=310,y=595)
-
 #photo
 image=Image.open("/Users/dell/Downloads/5.png")
 photo= ImageTk.PhotoImage(image)
